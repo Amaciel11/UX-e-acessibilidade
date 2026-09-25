427eeb0d81fdd7dda7b1d9a2308a2e9a360a0e12
Amaciel11 <maciel.arthur@escola.pr.gov.br>
2026-09-25 09:28:49 -0300

    salvo

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnAjuda = document.queryScelector(".botao-ajuda");
+let btnFechar = document.queryScelector(".botao-fechar");
+let modal= document.queryScelector(".modal-fundo");
+
+btnAjuda.addEventlistener("click",abremodal);
